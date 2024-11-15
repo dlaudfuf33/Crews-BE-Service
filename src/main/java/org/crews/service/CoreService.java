7f7e1fc162dc3835506b9d8da5ff7f1a05c00c5a
bw02184 <bw02184@naver.com>
2024-11-15 17:50:24 +0900

    feat:멤버 계좌 전체 조회 구현(확인 요망

--- src/main/java/org/crews/service/CoreService.java
+++ src/main/java/org/crews/service/CoreService.java
@@ -239,4 +239,25 @@ public class CoreService {
             throw new WebServerException(ex.getResponseBodyAsString(), ex);
         }
     }
+
+    public AccountInfoResponse accountDetails(CIOnlyRequest ci) { try {
+        AccountInfoResponse response = webClient.post()
+                .uri("/v1/accounts/info")
+                .headers(headers -> {
+                    headers.set(HEADER_ACCESS_KEY, accessKey);
+                    headers.set(HEADER_SECRET_KEY, secretKey);
+                })
+                .bodyValue(ci)
+                .retrieve()
+                .bodyToMono(AccountInfoResponse.class)
+                .block();
+        if(response == null)
+            throw new IllegalStateException("잘못된 응답값 입니다.");
+        return response;
+    }
+    catch (WebClientResponseException ex){
+        log.warn("클라이언트 통신 중 오류가 발생했습니다.{}", ex.getMessage());
+        throw new WebServerException(ex.getResponseBodyAsString(), ex);
+    }
+    }
 }
