be509cc5e9dcc87b6d46b573f11a4195d79b3bc5
kimchang0 <newshfkfk@gmail.com>
2024-11-25 17:42:41 +0900

    [Feat#44]: For_ReIssue_Response

--- src/main/java/org/crews/dto/response/ReissueResponse.java
+++ src/main/java/org/crews/dto/response/ReissueResponse.java
@@ -1,4 +1,10 @@
 package org.crews.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ReissueResponse {
+    private String message;
 }
