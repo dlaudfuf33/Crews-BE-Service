e0bf96321e7c081709247bdb8645fa12454715b6
kh-0818 <lkh3179@naver.com>
2024-12-06 11:46:08 +0900

    [fix/#76]: pull dev

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -364,7 +364,7 @@ VALUES ('1', 5000.00, NOW(), NOW(), 1),
 
 
 -- Dues 테이블에 더미 데이터 삽입
-INSERT INTO dues (due_date, created_at, due_amount, updated_at, membership_id, common_dues_id, is_payed, product_name,
+INSERT INTO dues (due_date, created_at, due_amount, updated_at, membership_id, common_dues_id, is_paid, product_name,
                   account_number, agit_name)
 VALUES (NOW(), NOW(), 1000.00, NOW(), 1, 1, FALSE, '우리 일반통장 상품', '1234', '독서 하는 모임'),
        (NOW(), NOW(), 2000.00, NOW(), 1, 2, FALSE, '우리 일반통장 상품', '1234', '러닝하는 클럽'),
