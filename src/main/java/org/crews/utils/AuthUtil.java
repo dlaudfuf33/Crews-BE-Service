6778dbaf2b98d87343cbea63045df5e14d9b2729
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-12-07 12:06:21 +0900

    [refactor/#90]: 임시비번, 인증번호 문자 포멧팅 추가

--- src/main/java/org/crews/utils/AuthUtil.java
+++ src/main/java/org/crews/utils/AuthUtil.java
@@ -15,7 +15,7 @@ public class AuthUtil {
     private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
     private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String DIGITS = "0123456789";
-    private static final String SPECIAL_CHARACTERS = "!@#$%^&*()-_=+[]{}|;:,.<>?/";
+    private static final String SPECIAL_CHARACTERS = "!@#$%^&*-+";
     private static final String ALL_CHARACTERS = LOWERCASE + UPPERCASE + DIGITS + SPECIAL_CHARACTERS;
 
     public AuthUtil(JWTUtil jwtUtil) {
@@ -40,8 +40,7 @@ public class AuthUtil {
         StringBuilder password = new StringBuilder();
 
         for (int i = 0; i < length; i++) {
-            int index = random.nextInt(ALL_CHARACTERS.length());
-            password.append(ALL_CHARACTERS.charAt(index));
+            password.append(ALL_CHARACTERS.charAt(random.nextInt(ALL_CHARACTERS.length())));
         }
 
         return password.toString();
