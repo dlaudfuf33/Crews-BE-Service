f8ff1f545cae94ecb949d95ae1fcdfa9fda4e070
bw02184 <bw02184@naver.com>
2024-12-10 17:22:34 +0900

    fix:qr생성 /추가

--- src/main/java/org/crews/config/AmazonConfig.java
+++ src/main/java/org/crews/config/AmazonConfig.java
@@ -3,7 +3,6 @@ package org.crews.config;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
