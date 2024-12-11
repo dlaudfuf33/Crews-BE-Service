48bf5a0bf4f176e7a0753fdd8f01ebb3ee9e6c18
bw02184 <bw02184@naver.com>
2024-12-11 13:30:26 +0900

    feat: member 가입시 nickName필드 삭제

--- src/main/java/org/crews/dto/request/MemberRequest.java
+++ src/main/java/org/crews/dto/request/MemberRequest.java
@@ -10,9 +10,6 @@ import lombok.ToString;
 @Getter
 @ToString
 public class MemberRequest {
-    @NotBlank(message = "닉네임은 필수 입력 항목입니다.")
-    private String nickName;
-
     @Email(message = "유효한 이메일 주소를 입력해주세요.")
     @NotBlank(message = "이메일은 필수 입력 항목입니다.")
     private String email;
